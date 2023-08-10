fb81ea18205289d43b7cf73348c5ec356a370944
Nguyễn Khải Tường <khaituong2909@gmail.com>
2023-08-10 13:12:36 +0700

    Fix error useBodyScrollLock

--- app/hooks/useBodyScrollLock.ts
+++ app/hooks/useBodyScrollLock.ts
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react"
 
 const useBodyScrollLock = (): [boolean, () => void] => {
-  const bodyStyle: CSSStyleDeclaration = document.body.style
-
-  const [isLocked, setIsLocked] = useState<boolean>(bodyStyle.overflowY === 'hidden')
+  const [isLocked, setIsLocked] = useState<boolean>(false)
 
   const toggle = (): void => setIsLocked(!isLocked)
+
   useEffect(() => {
+    const bodyStyle: CSSStyleDeclaration = document.body.style
     bodyStyle.overflowY = isLocked ? 'hidden' : 'auto'
-    
+    bodyStyle.paddingRight = isLocked ? '17px' : '0'
+
+    return () => {
+      bodyStyle.overflowY = 'auto'
+      bodyStyle.paddingRight = '0'
+    }
+
   }, [isLocked, toggle]);
 
   return [isLocked, toggle]
