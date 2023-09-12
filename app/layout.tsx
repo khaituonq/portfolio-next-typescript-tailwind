4169789a0041b563eb41e21cae7d3417f392ccf7
Nguyễn Khải Tường <khaituong2909@gmail.com>
2023-09-12 09:03:22 +0700

    Update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "This is my portfolio, I'am a fullstack web developer",
+  description:
+    "I'm a full-stack programmer from Vietnam by the name of Tuong, and I have expertise creating and developing websites. I am able to design websites that offer a smooth user experience across desktop and mobile platforms. My strategy places a strong emphasis on user usability, quick page loads, and general optimization. Check out my portfolio and hiring me will be your best choice.",
   icons: {
     icon: ["/images/favicon/favicon.ico"],
     apple: ["/images/apple-touch-icon/png"],
